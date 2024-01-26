825f0327bfae57a3903b29521678aaab8490843f
Coletta Uliano <culiano@live.com>
2024-01-26 15:44:34 +0000

    layout changes to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,12 +24,29 @@ function App() {
                 </div>
             </div> */}
             <h1>React Button Component</h1>
-            <h2>Button Variants</h2>
-            <h2>Border Radius</h2>
-            <h2>Icon Usage & Placement</h2>
-            <h2>Additional Style Utilities</h2>
-            <h3>Uppercase Text</h3>
-            <h3>Disabled</h3>
+            <div className='row'>
+                <div className='col-6'>
+                    <h2>Button Variants</h2>
+                </div>
+                <div className='col-6'>
+                    <h2>Border Radius</h2>
+                </div>
+                <div className='col-6'>
+                    <h2>Icon Usage & Placement</h2>
+                </div>
+                <div className='col-12'>
+                    <h2>Additional Style Utilities</h2>
+                    <div className='row'>
+                        <div className='col-6'>
+                            <h3>Uppercase Text</h3>
+                        </div>
+                        <div className='col-6'>
+                            <h3>Disabled</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                        
             <div  style={{display: 'flex'}}>
                 <div style={{margin: '0 5px'}}><Button variant='text' color='success' text="First Button" icon={['fas', 'camera-retro']} /></div>
                 <div style={{margin: '0 5px'}}><Button text="Second Button" variant='contained' icon={['far', 'envelope']} /></div>
